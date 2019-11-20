4961bee30f610066c59f997c5212e433e7658938
Hannu Örn <hannu.orn@iki.fi>
2019-11-20 13:45:28 +0200

    Sign up form, creating new users

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,9 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField
 
+
 class LoginForm(FlaskForm):
     username = StringField("Username")
     password = PasswordField("Password")
 
     class Meta:
         csrf = False
+
+
+class SignupForm(FlaskForm):
+    fullname = StringField("Full name")
+    username = StringField("Username")
+    password = PasswordField("Password")
+    password_repeated = PasswordField("Repeat password")
+
+    class Meta:
+        csrf = False
+
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,10 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, SignupForm
+
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -27,4 +28,37 @@ def auth_login():
 @app.route("/auth/logout")
 def auth_logout():
     logout_user()
+    return redirect(url_for("index"))
+
+
+@app.route("/auth/signup", methods = ["GET", "POST"])
+def auth_signup():
+
+    if request.method == "GET":
+        return render_template("auth/signupform.html", form = SignupForm())
+
+    form = SignupForm(request.form)
+
+    if not form.validate():
+        return render_template("auth/signupform.html", form = form)
+
+    user = User.query.filter_by(username=form.username.data).first()
+
+    if user:
+        return render_template("auth/signupform.html", form = form,
+                               error = "Username already exists")
+
+    if form.password.data != form.password_repeated.data:
+        return render_template("auth/signupform.html", form = form,
+                               error = "Passwords do not match")
+
+    u = User(form.fullname.data, form.username.data, form.password.data)
+    u.password = form.password.data
+
+    db.session().add(u)
+    db.session().commit()
+
+    user = User.query.filter_by(username=form.username.data).first()
+    login_user(user)
+
     return redirect(url_for("index"))
