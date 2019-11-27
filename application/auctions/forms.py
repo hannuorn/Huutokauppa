8606a46fd6911723c43a23a1e3d1a70aa4d6f561
Hannu Örn <hannu.orn@iki.fi>
2019-11-27 17:14:50 +0200

    Validations for AuctionForm and BiddingForm

--- application/auctions/forms.py
+++ application/auctions/forms.py
@@ -2,16 +2,36 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, validators
 
 class AuctionForm(FlaskForm):
-    title = StringField("Title", [validators.Length(min=5)])
-    description = StringField("Description", [validators.Length(min=5)])
-    minimum_bid = IntegerField("Minimum bid")
+
+    title = StringField("Title", 
+            [validators.Length(
+                    min = 5,
+                    max = 50),
+             validators.DataRequired()
+            ])
+
+    description = StringField("Description",
+            [validators.Length(
+                    min = 5,
+                    max = 200)
+            ])
+
+    minimum_bid = IntegerField("Minimum bid",
+            [validators.NumberRange(
+                    min = 1,
+                    max = 1000)
+            ])
 
     class Meta:
         csrf = False
 
 
 class BiddingForm(FlaskForm):
-    amount = IntegerField("Bid amount")
+    amount = IntegerField("Bid amount",
+            [validators.NumberRange(
+                    min = 1,
+                    max = 1000000)
+            ])
 
     class Meta:
         csrf = False
