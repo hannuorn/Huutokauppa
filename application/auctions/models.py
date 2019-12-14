c34466e4294fffa1cd32886e088c79156e6c9d00
Hannu Örn <hannu.orn@iki.fi>
2019-12-14 15:26:05 +0200

    Improve auction lists

--- application/auctions/models.py
+++ application/auctions/models.py
@@ -33,59 +33,51 @@ class Auction(Base):
 
 
     @staticmethod
-    def find_auctions():
-        stmt = text("SELECT auction.id, auction.title, MAX(bid.amount)"
-                    " FROM auction LEFT JOIN bid ON bid.auction_id = auction.id"
-                    " GROUP BY auction.id;")
-        res = db.engine.execute(stmt)
-
+    def parse_auctions(res):
         response = []
         for row in res:
-            hb = 0
+            highest_bid = 0
+            highest_bidder = ""
             if type(row[2]) == int:
-                hb = row[2]
+                highest_bid = row[2]
+                highest_bidder = row[3]
 
-            response.append({"id":row[0], "title":row[1], "highest_bid":hb})
+            response.append(
+                {"id":row[0],
+                "title":row[1],
+                "highest_bid":highest_bid,
+                "highest_bidder":highest_bidder,
+                "date_ends":row[4]})
 
         return response
 
 
     @staticmethod
     def find_active_auctions():
-        stmt = text("SELECT auction.id, auction.title, MAX(bid.amount)"
+        stmt = text("SELECT auction.id, auction.title, bid.amount, account.name, auction.date_ends"
                     " FROM auction LEFT JOIN bid ON bid.auction_id = auction.id"
-                    " WHERE auction.date_ends >= CURRENT_TIMESTAMP"
-                    " GROUP BY auction.id;")
+                    " LEFT JOIN account ON account.id = bid.account_id"
+                    " WHERE (auction.date_ends > CURRENT_TIMESTAMP) AND"
+                    " (bid.amount IS NULL or (bid.amount ="
+                    "    (SELECT MAX(bid.amount) FROM bid"
+                    "     WHERE bid.auction_id = auction.id)))"
+                    " ORDER BY auction.date_ends;")
         res = db.engine.execute(stmt)
-
-        response = []
-        for row in res:
-            hb = 0
-            if type(row[2]) == int:
-                hb = row[2]
-
-            response.append({"id":row[0], "title":row[1], "highest_bid":hb})
-
-        return response
+        return Auction.parse_auctions(res)
 
 
     @staticmethod
     def find_ended_auctions():
-        stmt = text("SELECT auction.id, auction.title, MAX(bid.amount)"
+        stmt = text("SELECT auction.id, auction.title, bid.amount, account.name, auction.date_ends"
                     " FROM auction LEFT JOIN bid ON bid.auction_id = auction.id"
-                    " WHERE auction.date_ends < CURRENT_TIMESTAMP"
-                    " GROUP BY auction.id;")
+                    " LEFT JOIN account ON account.id = bid.account_id"
+                    " WHERE (auction.date_ends < CURRENT_TIMESTAMP) AND"
+                    " (bid.amount IS NULL or (bid.amount ="
+                    "    (SELECT MAX(bid.amount) FROM bid"
+                    "     WHERE bid.auction_id = auction.id)))"
+                    " ORDER BY auction.date_ends DESC;")
         res = db.engine.execute(stmt)
-
-        response = []
-        for row in res:
-            hb = 0
-            if type(row[2]) == int:
-                hb = row[2]
-
-            response.append({"id":row[0], "title":row[1], "highest_bid":hb})
-
-        return response
+        return Auction.parse_auctions(res)
 
 
     @staticmethod
